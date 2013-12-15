1100909e5904fdd60d967f2fd6ec2b2fa79fd708
Nicolas Martyanoff <khaelin@gmail.com>
2013-12-15 19:55:46 +0100

    initial import

---
+++ GNUmakefile
@@ -0,0 +1,59 @@
+# Common
+prefix= /usr/local
+bindir= $(prefix)/bin
+
+CC=   clang
+
+CFLAGS+= -std=c99
+CFLAGS+= -Wall -Wextra -Werror -Wsign-conversion
+CFLAGS+= -Wno-unused-parameter -Wno-unused-function
+
+LDFLAGS=
+
+# Platform specific
+platform= $(shell uname -s)
+
+ifeq ($(platform), Linux)
+	CFLAGS+= -DSIRCC_PLATFORM_LINUX
+	CFLAGS+= -D_POSIX_C_SOURCE=200809L -D_BSD_SOURCE
+endif
+
+# Debug
+debug=0
+ifeq ($(debug), 1)
+	CFLAGS+= -g -ggdb
+else
+	CFLAGS+= -O2
+endif
+
+# Target: sircc
+sircc_BIN= sircc
+sircc_SRC= $(wildcard src/*.c)
+sircc_OBJ= $(subst .c,.o,$(sircc_SRC))
+
+$(sircc_BIN): CFLAGS+=
+$(sircc_BIN): LDFLAGS+=
+$(sircc_BIN): LDLIBS+= -lncurses
+
+# Rules
+all: bin
+
+bin: $(sircc_BIN)
+
+$(sircc_BIN): $(sircc_OBJ)
+	$(CC) $(LDFLAGS) -o $@ $< $(LDLIBS)
+
+clean:
+	$(RM) $(sircc_BIN) $(wildcard src/*.o)
+
+install: bin
+	mkdir -p $(bindir)
+	install -m 755 $(sircc_BIN) $(bindir)
+
+uninstall:
+	$(RM) $(addprefix $(bindir)/,$(sircc_BIN))
+
+tags:
+	ctags -o .tags -a $(wildcard sircc/*.[hc])
+
+.PHONY: all bin clean install uninstall tags
---
+++ src/sircc.c
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2013 Nicolas Martyanoff
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <unistd.h>
+
+static void die(const char *, ...)
+    __attribute__((format(printf, 1, 2)));
+static void usage(const char *, int);
+
+int
+main(int argc, char **argv) {
+    int opt, nb_args;
+
+    opterr = 0;
+    while ((opt = getopt(argc, argv, "h")) != -1) {
+        switch (opt) {
+        case 'h':
+            usage(argv[0], 0);
+            break;
+
+        case '?':
+            usage(argv[0], 1);
+        }
+    }
+
+    nb_args = argc - optind;
+
+    return 0;
+}
+
+static void
+usage(const char *argv0, int exit_code) {
+    printf("Usage: %s [-h]\n"
+            "\n"
+            "Options:\n"
+            "  -h         display help\n",
+            argv0);
+    exit(exit_code);
+}
+
+static void
+die(const char *fmt, ...) {
+    va_list ap;
+
+    fprintf(stderr, "fatal error: ");
+
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    va_end(ap);
+
+    putc('\n', stderr);
+    exit(1);
+}
